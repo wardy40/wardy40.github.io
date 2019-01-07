b8c13c986953be79f52d2a8489fbfb11ba2c4742
Jason Wardy <jason.wardy@gmail.com>
2019-01-06 19:32:32 -0500

    moved to bootstrap template
    completely different site

---
+++ archive/site_files/script.js
@@ -0,0 +1,12 @@
+function toggleRoadmap(obj) {
+    
+    if (obj.innerHTML == "Hide") {
+        document.getElementById("roadmap").getElementsByTagName("h3")[0].style.display = "none"
+        document.getElementById("roadmap").getElementsByTagName("ul")[0].style.display = "none"
+        obj.innerHTML = "Show roadmap"
+    } else {
+        document.getElementById("roadmap").getElementsByTagName("h3")[0].style.display = "block"
+        document.getElementById("roadmap").getElementsByTagName("ul")[0].style.display = "block"
+        obj.innerHTML = "Hide"
+    }
+};
